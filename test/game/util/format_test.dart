814c511c41cab3e1240fb9ffe92ba2c4b564f04e
cwchanap <cwchanap@connect.ust.hk>
2026-07-29 08:54:25 -0700

    refactor: promote percent/number formatters to util/format (HPA-103)

---
+++ test/game/util/format_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:orion/game/util/format.dart';
+
+void main() {
+  test('percent rounds to nearest whole percent', () {
+    expect(percent(0.5), '50%');
+    expect(percent(0.424), '42%');
+    expect(percent(1.2), '120%');
+  });
+
+  test('number drops trailing .0 for whole values, else one decimal', () {
+    expect(number(2.0), '2');
+    expect(number(2.5), '2.5');
+    expect(number(0.45), '0.5');
+  });
+}
