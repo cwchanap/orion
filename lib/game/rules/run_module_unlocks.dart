704d94a546d5e289a3297b944271cd4311c243d4
cwchanap <cwchanap@connect.ust.hk>
2026-08-11 05:07:42 -0700

    feat: define first boss blueprint module

---
+++ lib/game/rules/run_module_unlocks.dart
@@ -0,0 +1,22 @@
+import '../campaign/campaign_progress.dart';
+import '../campaign/orion_campaign.dart';
+import '../models/game_models.dart';
+
+const _unlockStageByModule = <RunModuleId, String>{
+  RunModuleId.relayCalibration: OrionCampaign.stageOneId,
+};
+
+abstract final class RunModuleUnlocks {
+  static bool hasFirstBlueprint(CampaignProgress progress) =>
+      progress.isCleared(OrionCampaign.stageOneId);
+
+  static Set<RunModuleId> availableFor(CampaignProgress progress) =>
+      Set<RunModuleId>.unmodifiable(
+        runModuleCatalog.map((definition) => definition.id).where((id) {
+          final unlockStageId = _unlockStageByModule[id];
+          return unlockStageId == null || progress.isCleared(unlockStageId);
+        }),
+      );
+
+  static final Set<RunModuleId> baseModules = availableFor(CampaignProgress());
+}
